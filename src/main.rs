5d30c6285c4828424a6ace43299fc1c39edc6ad1
uemoo <uemoo96122@gmail.com>
2022-02-12 14:31:18 +0900

    feat: 引数を渡せるように修正

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,23 @@ use std::{io::{stdin, stdout, Write}, process::Command};
 
 fn main(){
     loop {
-        // use the `>` character as the prompt
-        // need to explicitly flush this to ensure it prints before read_line
         print!("> ");
         stdout().flush().unwrap();
 
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
 
-        let command = input.trim();
+        // everything after the first whitespace character 
+        //     is interpreted as args to the command
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
 
         let mut child = Command::new(command)
+            .args(args)
             .spawn()
             .unwrap();
 
-        // don't accept another command until this one completes
-        child.wait().unwrap(); 
+        child.wait().unwrap();
     }
 }
